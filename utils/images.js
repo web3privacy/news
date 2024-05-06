ec25d79ab39a73e8648979e83e6b915ab74a20d9
tree <tree@hidden.cz>
2024-05-07 01:05:47 +0200

    fix img gen

--- utils/images.js
+++ utils/images.js
@@ -1,30 +1,36 @@
 import { join } from "https://deno.land/std@0.208.0/path/mod.ts";
-import { emptyDir } from "https://deno.land/std@0.196.0/fs/empty_dir.ts";
+import { ensureDir } from "https://deno.land/std@0.224.0/fs/ensure_dir.ts";
+import { existsSync } from "https://deno.land/std@0.224.0/fs/exists.ts";
 
-const DEST_DIR = "./dist";
+const DATA_DIR = "./data";
 
-const imgDir = join(DEST_DIR, "img");
-await emptyDir(imgDir);
+const imgDir = "./web/public/img";
+await ensureDir(imgDir);
 
-const issues = JSON.parse(
-  await Deno.readTextFile(join(DEST_DIR, "index.json")),
-);
 
 // get images
-for (const issue of issues) {
-  await genImage(
-    `https://news.web3privacy.info/image/${issue.week}?${new Date().valueOf()}`,
-    join(imgDir, `${issue.week}.png`),
-  );
+for (const { name: year } of Deno.readDirSync(DATA_DIR)) {
+  for (const { name: fn } of Deno.readDirSync(join(DATA_DIR, year))) {
+    const week = fn.match(/^week(\d+)/)?.[1];
+    if (!week) continue;
+    const yearWeek = `${year}-${week}`;
+    const imgn = `https://news.web3privacy.info/image/${yearWeek}?${new Date().valueOf()}`;
+    const outputFn = join(imgDir, `${yearWeek}.png`);
+    if (existsSync(outputFn)) continue;
+    console.log('Processing:', imgn);
+    await genImage(imgn, outputFn);
+  }
 }
 
 // make cover
-await genImage(
-  `https://news.web3privacy.info/cover`,
-  join(imgDir, "cover.png"),
-);
+const coverFn = join(imgDir, "cover.png");
+if (!existsSync(coverFn)) {
+  await genImage(`https://news.web3privacy.info/cover`, coverFn);
+}
 
 console.log("Done");
+
+// ---------
 
 async function genImage(url, fn) {
   const imgResp = await fetch("https://html2svg.gwei.cz", {
@@ -35,6 +41,9 @@ async function genImage(url, fn) {
       width: 1920,
       height: 960,
     }),
+    headers: {
+      "content-type": "application/json",
+    }
   });
 
   if (imgResp.body) {
